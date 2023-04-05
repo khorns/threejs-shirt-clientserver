cdb5ceb9d93fec339fa2db012327a26769c9ff73
Sereyvathanak Khorn <khorn.vathanak@gmail.com>
2023-04-05 13:48:53 -0700

    Adding Both client and server

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import CanvasModel from './canvas'
+import Customizer from './pages/Customizer'
+import Home from './pages/Home'
+
+
+function App() {
+  return (
+    <main className='app transition-all'>
+      <Home />
+      <CanvasModel />
+      <Customizer />
+    </main>
+    )
+}
+ 
+export default App
